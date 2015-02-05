184134b520b5cf85c550753560612a78d5088584
cocodro <combswc@gmail.com>
2015-02-05 18:35:51 -0500

    Fixed Issue with Task Form

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,6 @@ class Task < ActiveRecord::Base
   belongs_to :list
 
   def days_left
-    (6 - (DateTime.now.to_date - created_at.to_date)).to_i
+    (7 - (DateTime.now.to_date - created_at.to_date)).to_i
   end
 end
